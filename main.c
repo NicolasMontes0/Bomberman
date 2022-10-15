4cf51a7ef26fd4abf017584854cac629d237649f
HaJeremy <jeremyha2002@gmail.com>
2022-10-15 15:29:05 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int choix = 0;
+    printf("Veuillez choisir entre les different choix\n"
+           "1-Demarrer \n"
+           "2-Demarrer le serveur\n"
+           "3-Rejoindre un serveur\n");
+    scanf("%d", &choix);
+    printf("%d",choix);
+/*
+    switch (choix = 1) {
+        case :
+
+    }
+    switch (choix = 2) {
+        case :
+    }
+    switch (choix = 3) {
+        case :
+
+    }
+    */
+}
