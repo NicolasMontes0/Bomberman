597818f4ab9d0a83b67b9b8497afd7e68cb2a9f2
Thavfyk2 <112617906+Thavfyk2@users.noreply.github.com>
2022-11-20 13:57:58 +0100

    Update main.c

--- main.c
+++ main.c
@@ -2,60 +2,123 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include "Personnage.h"
+#include "Map.c"
 
-const char murIndestructible = 'x';
-const char murDestructible = 'm';
-const char Joueur = 'p';
-const char espace = ' ';
+// PNJ BOT
+void search(char objet) {
+    int xSize = 5;
+    int ySize = 5;
+    int yPlayerPos;
+    int xPlayerPos;
+    int playerFound = 0;
+    int xObjPos;
+    int yObjPos;
+    int objFound = 0;
 
-void afficherMap(Map map) {
-    printf("%d\n%d %d\n", map.nbBombes,map.longueur,map.hauteur);
-    for (int i = 0; i < map.hauteur; ++i) {
-        for (int j = 0; j < map.longueur; ++j) {
-            printf("%c", *(*(map.map + i) + j));
+    //TODO : Attention taille des tableaux a revoir
+
+    char tab[5][5] = {{'x', 'x', 'x', 'x', 'x'},
+                      {'x', ' ', ' ', ' ', 'x'},
+                      {'x', 'm', 'p', ' ', 'x'},
+                      {'x', ' ', ' ', ' ', 'x'},
+                      {'x', 'x', 'x', 'x', 'x'},};
+
+    for (int y = 0; y < ySize; y++) {
+        for (int x = 0; x < xSize; x++) {
+            if (tab[y][x] == 'p') {
+                yPlayerPos = y;
+                xPlayerPos = x;
+                playerFound = 1;
+                break;
+            }
         }
-        printf("\n");
+        if (playerFound == 1) {
+            break;
+        }
+    }
+    int i = 1;
+    while (objFound == 0) {
+        if (tab[yPlayerPos][i] == objet) {
+
+        }
     }
 }
 
-char **mapUn(int id, int longueur, int hauteur, int nbBombes) {
-    char** carte = malloc(sizeof(char*) * longueur);
+void start() {
 
-    for (int a = 0; a < longueur; ++a) {
-        carte[a] = malloc(sizeof(char) * hauteur);
-    }
+    Map map = {1, 7, 5, 2, NULL};
 
-    for (int i = 0; i < hauteur; ++i) {//hauteur / ligne
+    Personnage Joueur1 = {1, 1, 2};
+    Personnage Joueur2 = {map.hauteur - 2, map.longueur - 2, 1};
 
-        for (int j = 0; j < longueur; ++j) {//longueur / colonne
+    map.map = mapUn(map.id, map.longueur, map.hauteur, map.nbBombes);
 
-
-            if (i == 0 || i == hauteur - 1 || j == 0 ||
-                j == longueur - 1) {                                                             //Bordure Indestructible
-
-                *(*(carte + i) + j) = murIndestructible;
-            }
-            else if ((i == 1 && j == 1) || (i == hauteur - 2 && j == longueur - 2)) {
-
-                *(*(carte + i) + j) = Joueur;
-
-                if (*(*(carte + i - 1) + j) != murIndestructible) {
-                    *(*(carte + i - 1) + j) = espace;
-                }
-                if (*(*(carte + i) + j - 1) != murIndestructible) {
-                    *(*(carte + i) + j - 1) = espace;
-                }
-                if (*(*(carte + i + 1) + j) != murIndestructible) {
-                    *(*(carte + i + 1) + j) = espace;
-                }
-                if (*(*(carte + i) + j + 1) != murIndestructible) {
-                    *(*(carte + i) + j + 1) = espace;
-                }
-            }
-            else if (*(*(carte + i) + j) != espace) {
-                *(*(carte + i) + j) = murDestructible;
+    Map maps[] = {map};
+    bool selection[] = {false};
+    bool selectionEnd = false;
+    do {
+        size_t nbMap = sizeof(maps) / sizeof(Map);
+        for (int i = 0; i < nbMap; i++) {
+            printf("Map-%d:\n", i + 1);
+            afficherMap(maps[i]);
+            if (selection[i] == true) {
+                printf("Selected\n\n");
+            } else {
+                printf("Not selected\n\n");
             }
         }
+        int choice = 0;
+        printf("Veuillez selectionner la ou les carte(s) par leur numeros (Tapez 0 pour finir la selection):\n");
+        scanf("%d", &choice);
+
+        if (choice == 0) {
+            int selected = 0;
+            for (int i = 0; i < nbMap; i++) {
+                if (selection[i] == true) {
+                    selected++;
+                }
+            }
+            if (selected == 0) {
+                printf("Veuillez selectionner au moins une map !\n");
+            } else {
+                selectionEnd = true;
+            }
+        }
+
+        if (choice <= nbMap && selection[choice - 1] == true) {
+            selection[choice - 1] = false;
+        } else if (choice <= nbMap && selection[choice - 1] == false) {
+            selection[choice - 1] = true;
+        }
+
+        printf("\n");
+    } while (selectionEnd == false);
+}
+
+int main() {
+    int choix = 1;
+    while (choix > 0 && choix < 4) {
+        printf("Veuillez selectionner une action:\n"
+               "1-Demarrer \n"
+               "2-Demarrer le serveur\n"
+               "3-Rejoindre un serveur\n"
+               "Autre-Quitter le jeux\n");
+        scanf("%d", &choix);
+
+        switch (choix) {
+            case 1:
+                printf("Vous commencez une partie en solo...\n");
+                start();
+                break;
+            case 2:
+                printf("Vous ouvrez un serveur...\n");
+                break;
+            case 3:
+                printf("Vous vous connecter au serveur...\n");
+                break;
+            default:
+                exit(EXIT_FAILURE);
+        }
     }
-    return carte;
 }
